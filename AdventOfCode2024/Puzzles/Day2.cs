3a851ad4f4a5700f2173e2113fe64cf31ddf3dfb
Diamond3 <diamond322222@gmail.com>
2024-12-02 22:06:55 +0200

    Add Day2-2

--- AdventOfCode2024/Puzzles/Day2.cs
+++ AdventOfCode2024/Puzzles/Day2.cs
@@ -8,28 +8,50 @@ internal class Day2 : ISolver
     {
         using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
         var safeReportsCount = 0;
-        
-        while(stream.ReadLine() is string line)
+
+        while (stream.ReadLine() is string line)
         {
             var arr = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-            var direction = Math.Sign(arr[1] - arr[0]);
-            var isSafe = false;
-
-            for (int i = 1; i < arr.Length; i++)
+            if (IsValidReport(arr, -1)
+                || Enumerable.Range(0, arr.Length).Any(i => IsValidReport(arr, i)))
             {
-                var diff = arr[i] - arr[i - 1];
-                if (Math.Abs(diff) > 3 || direction != Math.Sign(diff))
-                {
-                    isSafe = false;
-                    break;
-                }
-                isSafe = true;
+                safeReportsCount++;
             }
-
-            _ = isSafe ? safeReportsCount++ : 0;
         }
 
         return safeReportsCount.ToString();
+    }
+
+    // skip => 0 -> l1 r2, 1 -> l0 r2, 2 -> l0, r1, 3 -> l0 ...
+
+    /* skip == 0 => x 1 2 3 4
+     * skip == 1 => 0 x 2 3 4
+     * skip == 2 => 0 1 x 3 4
+     */
+
+    private static bool IsValidReport(int[] arr, int skipInd)
+    {
+        var left = skipInd != 0 ? 0 : 1;
+        var right = skipInd != 1 ? left + 1 : left + 2;
+        var firstDirection = Math.Sign(arr[right] - arr[left]);
+
+        while (right < arr.Length)
+        {
+            if (right != skipInd)
+            {
+                var diff = arr[right] - arr[left];
+
+                if (Math.Abs(diff) > 3 || firstDirection != Math.Sign(diff))
+                {
+                    return false;
+                }
+
+                left = right;
+            }
+            right++;
+        }
+
+        return true;
     }
 }
