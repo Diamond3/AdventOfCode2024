307f2718f37b2e9e6911042a0e80484c3c8f062f
Deimantas Eidukevičius <deimantas.eidukevici@visma.com>
2024-12-03 09:58:15 +0200

    Add Day3-1

---
+++ AdventOfCode2024/Puzzles/Day3.cs
@@ -0,0 +1,62 @@
+﻿using AdventOfCode2024.Utils;
+
+namespace AdventOfCode2024.Puzzles;
+
+internal class Day3 : ISolver
+{
+    public string Solve()
+    {
+        using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
+
+        var wholeSum = 0L;
+
+        while (stream.ReadLine() is string line)
+        {
+            var isNumber = false;
+            
+
+            for (int i = 0; i < line.Length - 4; i++)
+            {
+                var j = i;
+                int a = 0, b = 0;
+
+                if (line.Substring(j, 4) == "mul(")
+                {
+                    j += 4;
+
+                    var c = 0;
+                    while(c < 3 && char.IsNumber(line[j]))
+                    {
+                        a = a * 10 + (line[j] - '0');
+                        j++;
+                        c++;
+
+                    }
+
+                    if (c == 0 || line[j++] != ',')
+                    {
+                        continue;
+                    }
+
+                    c = 0;
+                    while (c < 3 && char.IsNumber(line[j]))
+                    {
+                        b = b * 10 + (line[j] - '0');
+                        j++;
+                        c++;
+                    }
+
+                    if (c == 0 || line[j] != ')')
+                    {
+                        continue;
+                    }
+
+                    wholeSum += a * b;
+                    i = j;
+                }
+            }
+        }
+
+        return wholeSum.ToString();
+    }
+}
