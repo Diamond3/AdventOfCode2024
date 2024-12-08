4da9c7dc3b6f195302b2264de3e70aa83a4fffee
Diamond3 <diamond322222@gmail.com>
2024-12-08 09:36:47 +0200

    Add Day8-1

---
+++ AdventOfCode2024/Puzzles/Day8.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode2024.Utils;
+
+namespace AdventOfCode2024.Puzzles;
+
+internal class Day8 : ISolver
+{
+    public string Solve()
+    {
+        using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
+        var antennaPairs = new Dictionary<char, List<(int y, int x)>>();
+        var y = 0;
+        var mapWidth = 0;
+
+        while (stream.ReadLine() is string line)
+        {
+            mapWidth = line.Length;
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] != '.')
+                {
+                    if (!antennaPairs.ContainsKey(line[x]))
+                    {
+                        antennaPairs[line[x]] = [];
+                    }
+
+                    antennaPairs[line[x]].Add((y, x));
+                }
+            }
+            y++;
+        }
+
+        var mapHeight = y;
+        var antinodeSet = new HashSet<(int, int)>();
+
+        foreach (var list in antennaPairs.Values)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j <= list.Count - 1; j++)
+                {
+                    var dir = (y: list[i].y - list[j].y, x: list[i].x - list[j].x); // Dir from j to i
+                    var antinode = (y: list[i].y + dir.y, x: list[i].x + dir.x);
+
+                    if (antinode.x >= 0 && antinode.x < mapWidth && antinode.y >= 0 && antinode.y < mapHeight)
+                    {
+                        antinodeSet.Add(antinode);
+                    }
+
+                    antinode = (y: list[j].y - dir.y, x: list[j].x - dir.x);
+                    if (antinode.x >= 0 && antinode.x < mapWidth && antinode.y >= 0 && antinode.y < mapHeight)
+                    {
+                        antinodeSet.Add(antinode);
+                    }
+                }
+            }
+        }
+
+        return antinodeSet.Count.ToString();
+    }
+}
