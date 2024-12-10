aae3cbd93580e277a90a664b87e832ac08f8bf81
Diamond3 <diamond322222@gmail.com>
2024-12-11 00:20:29 +0200

    Add Day10-1

---
+++ AdventOfCode2024/Puzzles/Day10.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode2024.Utils;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace AdventOfCode2024.Puzzles;
+
+internal class Day10 : ISolver
+{
+    public string Solve()
+    {
+        using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
+
+        var map = new Dictionary<(int y, int x), int>();
+        var stack = new Stack<((int y, int x) pos, HashSet<(int x, int y)> visited, int score)>();
+        var directions = new (int y, int x)[]
+        {
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0)
+        };
+
+        var y = 0;
+        var trailheads = new List<HashSet<(int, int)>>();
+
+        while (stream.ReadLine() is string str)
+        {
+            for (int x = 0; x < str.Length; x++)
+            {
+                map[(y, x)] = str[x] - '0';
+                if (map[(y, x)] == 0)
+                {
+                    stack.Push(((y, x), new HashSet<(int x, int y)>() { (y, x) }, trailheads.Count));
+                    trailheads.Add(new HashSet<(int, int)>());
+                }
+            }
+            y++;
+        }
+
+        while (stack.Count > 0)
+        {
+            var (pos, visited, indx) = stack.Pop();
+            if (map[pos] == 9)
+            {
+                trailheads[indx].Add(pos);
+                continue;
+            }
+
+            foreach (var dir in directions)
+            {
+                var newPos = (pos.y + dir.y, pos.x + dir.x);
+
+                if (map.TryGetValue(newPos, out var cost)
+                    && map[pos] + 1 == cost
+                    && !visited.Contains(newPos))
+                {
+                    var newVisited = new HashSet<(int, int)>(visited)
+                    {
+                        newPos
+                    };
+
+                    stack.Push((newPos, newVisited, indx));
+                }
+            }
+        }
+        return trailheads.Sum(x => x.Count).ToString();
+    }
+}
