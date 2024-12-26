5726707cc29c973cf60d7aaf3953100eb0f0843e
Deimantas Eidukevicius <Diamond322222@gmail.com>
2024-12-26 15:35:11 +0200

    Add Day 25-1

--- AdventOfCode2024/Puzzles/Day24.cs
+++ AdventOfCode2024/Puzzles/Day24.cs
@@ -1,4 +1,6 @@
 ﻿using AdventOfCode2024.Utils;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.Intrinsics.Arm;
 
 namespace AdventOfCode2024.Puzzles;
 
@@ -8,15 +10,34 @@ internal class Day24 : ISolver
     {
         using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
 
-        var zBits = new PriorityQueue<string, int>();
+        var inputX = new List<string>();
+        var inputY = new List<string>();
+        var output = new List<string>();
+
+        var zBitsQueue = new PriorityQueue<string, int>();
         var wires = new Dictionary<string, int>();
         var gates = new Dictionary<string, (string left, string right, string operation)>();
+
+        var possibleSwaps = new (int a, int b, int c, int d)[]{
+            (1, 0, 3, 2),
+            (2, 3, 0, 1),
+            (3, 2, 1, 0)
+        };
 
         while (stream.ReadLine() is string line && !string.IsNullOrEmpty(line))
         {
             if (line.Split(": ", 2) is [var node, var value])
             {
                 wires[node] = int.Parse(value);
+                if (node[0] == 'x')
+                {
+                    inputX.Insert(0, node);
+                }
+
+                if (node[0] == 'y')
+                {
+                    inputY.Insert(0, node);
+                }
             }
         }
 
@@ -28,22 +49,44 @@ internal class Day24 : ISolver
                 gates[result] = (left, right, operation);
                 if(result[0] == 'z')
                 {
-                    zBits.Enqueue(result, 100 - int.Parse(result[1..]));
+                    zBitsQueue.Enqueue(result, 100 - int.Parse(result[1..]));
                 }
             }
         }
 
-        var ans = "";
-        while (zBits.Count > 0)
+        while (zBitsQueue.Count > 0)
         {
-            var key = zBits.Dequeue();
+            output.Add(zBitsQueue.Dequeue());
+        }
+
+        var xNum = Convert.ToInt64(string.Join("", inputX.Select(x => wires[x])), 2);
+        var yNum = Convert.ToInt64(string.Join("", inputY.Select(y => wires[y])), 2);
+        var expectedOutput = Convert.ToString(xNum + yNum, 2);
+
+        for (int i = 0; i < output.Count; i++)
+        {
+            var tempWires = new Dictionary<string, int>(wires);
+            var nodes = new HashSet<string>();
+            var a = GetValue(wires, gates, output[i], nodes);
+            if (a != expectedOutput[i])
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+
+        /*var ans = "";
+        for (int i = 0; i < output.Count; i++)
+        {
+            var key = output[i];
             var a = GetValue(wires, gates, key);
             wires[key] = a;
 
             ans += a;
         }
+        var b = ans.Length;*/
 
-        return Convert.ToInt64(ans, 2).ToString();
+        //return Convert.ToInt64(ans, 2).ToString();
+        return "";
     }
 
     private int GateOperationResult(int l, int r, string operation)
@@ -56,16 +99,17 @@ internal class Day24 : ISolver
         };
     }
 
-    private int GetValue(Dictionary<string, int> wires, Dictionary<string, (string left, string right, string operation)> gates, string node)
+    private int GetValue(Dictionary<string, int> wires, Dictionary<string, (string left, string right, string operation)> gates, string node, HashSet<string> nodes)
     {
+        nodes.Add(node);
         if (wires.ContainsKey(node))
         {
             return wires[node];
         }
 
         var (l, r, operation) = gates[node];
-        var left = GetValue(wires, gates, l);
-        var right = GetValue(wires, gates, r);
+        var left = GetValue(wires, gates, l, nodes);
+        var right = GetValue(wires, gates, r, nodes);
 
         wires[node] = GateOperationResult(left, right, operation);
 
---
+++ AdventOfCode2024/Puzzles/Day25.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode2024.Utils;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2024.Puzzles;
+
+internal class Day25 : ISolver
+{
+    public string Solve()
+    {
+        using var stream = new StreamReader($"Inputs/{GetType().Name}.txt");
+        var keys = new List<int[]>();
+        var locks = new List<int[]>();
+
+        while (stream.ReadLine() is string line)
+        {
+            var isLock = line.Contains('#');
+            var l = new int[5] { -1, -1, -1, -1, -1 };
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    l[j] += line[j] == '#' ? 1 : 0;
+                }
+                line = stream.ReadLine()!;
+            }
+
+            if (isLock)
+            {
+                locks.Add(l);
+            }
+            else
+            {
+                keys.Add(l);
+            }
+        }
+
+        var dict = new HashSet<int[]>();
+        var count = 0;
+        foreach (var key in keys)
+        {
+            foreach (var lo in locks)
+            {
+                var fits = true;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (lo[i] + key[i] > 5)
+                    {
+                        fits = false;
+                        break;
+                    }
+                }
+                if (fits)
+                {
+                    count++;
+                }
+            }
+        }
+        
+
+        return count.ToString();
+    }
+}
